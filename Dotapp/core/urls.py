8141e58dc4b1b7136eb67eb4d2935e23f9f11bbc
Joan Vargas <joanv313@gmail.com>
2025-11-10 02:58:56 -0500

    por fa

--- Dotapp/core/urls.py
+++ Dotapp/core/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('registro/', views.registro_aprendiz, name='registro-aprendiz'),
     path('login/', views.login_view, name='login'),
     path('manual/', views.manual, name='manual'),
     path('dashboard/', views.dashboard_admin, name='dashboard_admin'),
-    #recuperacion de contraseña
     path("reset/", views.password_reset_request, name="password_reset_request"),
     path("reset/<uidb64>/<token>/", views.password_reset_confirm, name="password_reset_confirm"),
     path("acceso_denegado/", views.acceso_denegado, name="acceso_denegado"),
